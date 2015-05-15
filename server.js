27b69d6830a33920ef764cd8f2ba575b8852621a
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2015-05-15 16:55:40 +0000

    Modular bot

--- server.js
+++ server.js
@@ -1,66 +1,26 @@
+'use strict';
+
 var Net = require('net');
 
-var server = {
-	'PORT' : 6667,
-	'ADDRESS' : 'irc.freenode.net'
-};
+var Config = require('./utils/config');
+var Channel = require('./utils/channel');
 
-var user = {
-	'NICK' : 'Skolpad',
-	'USER' : 'Skolpad',
-	'REAL' : 'Skolpad BOT',
-	'PASS' : 'Skolpad'
-};
-
-var chan = [
-	'#skolpad'
-];
-
-var commands = {
-	'!beep' : 'boop',
-	'!foo' : 'bar'
-};
-
-var irc = Net.connect(server.PORT, server.ADDRESS, function () {
-	console.log('Connecting to ' + server.ADDRESS + ':' + server.PORT);
+var irc = Net.connect(Config.PORT, Config.ADDRESS, function () {
+	console.log('Connecting to ' + Config.ADDRESS + ':' + Config.PORT);
 });
 
 irc.setEncoding('ascii');
 irc.setNoDelay();
 
-var response = function (stream) {
-	irc.write(stream + '\n');
-};
+exports.irc = irc;
 
-irc.on('data', function (stream) {
-	var streamString = stream.toString().trim();
-	var streamSplit = streamString.split(' ');
-
-	if (streamString.match(/^PING/)) {
-		console.log('Sending PONG');
-
-		return response('PONG');
-	}
-
-	for (var respond in commands) {
-		if (streamString.match(new RegExp(respond, 'i'))) {
-			var respondToChannel = streamSplit[2];
-			var respondToCommand = commands[respond.toString()];
-
-			console.log('Sending ' + respondToChannel + ' REPLY to ' + respondToCommand);
-
-			irc.emit(respondToCommand, respondToChannel);
-			response('PRIVMSG ' + respondToChannel + ' :' + respondToCommand);
-		}
-	}
-
-	console.log(streamString);
-});
+var Request = require('./lib/request');
+var Response = require('./lib/response');
 
 irc.on('connect', function () {
 	setTimeout(function () {
-		response('NICK ' + user.NICK);
-		response('USER ' + user.USER + ' 8 * :' + user.REAL);
-		response('JOIN ' + chan.join(','));
+		Response.write('NICK ' + Config.NICK);
+		Response.write('USER ' + Config.USER + ' 8 * :' + Config.REAL);
+		Response.write('JOIN ' + Channel.join(','));
 	}, 1000);
 });
---
+++ utils/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+	'PORT' : 6667,
+	'ADDRESS' : 'irc.freenode.net',
+	'NICK' : 'Skolpad',
+	'USER' : 'Skolpad',
+	'REAL' : 'Skolpad Bot v1.0',
+	'PASS' : 'Skolpad'
+};
